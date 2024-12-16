25be683ca98aaf5426c1d54b03a211e9cada15ef
Rigv Sarker <Rigv.Sarker@gmail.com>
2024-12-16 17:40:07 -0600

    practis

---
+++ src/java4Kids_variables/Method.java
@@ -0,0 +1,16 @@
+package java4Kids_variables;
+
+public class Method {
+	static String y="Hello world";
+	static int w=1000;
+	static int v=1000;
+	public static void main(String[]    args) {
+		String l="I love Java";
+		System.out.println(l);
+		System.out.println(y);
+		System.out.println(v+w);
+	}
+	
+	
+
+}
--- src/variables/ReviewClass_1to3.java
+++ src/variables/ReviewClass_1to3.java
@@ -5,15 +5,20 @@ public class ReviewClass_1to3 {
               static int f=133;
               static int s=144;
               static int d=222; 
-              static byte a=123;    
+              static byte a=123;   
+              static byte j=16;   
 	public static void main(String[] args) {
 		System.out.println("my ints");
 		System.out.println(x);	
 		System.out.println(f);
 		System.out.println(s);
 		System.out.println(d);
-		System.out.println("my bytes ");
+		System.out.println("my bytes");
 		System.out.println( a);  
+		System.out.println(j);  
+		
+		System.out.println("add");  
+		
 	}	
 	
 }
