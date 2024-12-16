370d30aa58f54990c2b31bfd5960f30fbe2f3b71
Rigv Sarker <Rigv.Sarker@gmail.com>
2024-12-15 18:15:58 -0600

    second commit

---
+++ src/variables/ReviewClass_1to3.java
@@ -0,0 +1,19 @@
+package variables;
+//variables
+public class ReviewClass_1to3 {
+              static int x=122;
+              static int f=133;
+              static int s=144;
+              static int d=222; 
+              static byte a=123;    
+	public static void main(String[] args) {
+		System.out.println("my ints");
+		System.out.println(x);	
+		System.out.println(f);
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println("my bytes ");
+		System.out.println( a);  
+	}	
+	
+}
