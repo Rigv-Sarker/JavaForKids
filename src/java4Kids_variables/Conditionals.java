7471059d2ad0e11327c42e72cc3377aeb9ca8b03
Rigv Sarker <Rigv.Sarker@gmail.com>
2025-01-07 20:13:22 -0600

    learned conditionls

---
+++ src/java4Kids_variables/Conditionals.java
@@ -0,0 +1,20 @@
+package java4Kids_variables;
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+
+		int x = 110;  // your number 
+		int y = 121;  // comparison number
+		
+		if(x==y) {
+			System.out.println("Numbers are equal!");
+		}else if (x < y) {
+			System.out.println("Your number is " + x + " and lesser than " + y);
+		} else {
+			System.out.println("Your number is " + x + " and greater than " + y);
+		}
+
+	}
+
+}
---
+++ src/java4Kids_variables/Loop.java
@@ -0,0 +1,20 @@
+package java4Kids_variables;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		int x = 1000;
+		
+//		System.out.println(x);
+//		x = x-1;  // 23  
+//		System.out.println(x);
+//		x = x-1;  // 22
+//		System.out.println(x);
+		
+		for(int i=1; i<= x; i++  ) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
