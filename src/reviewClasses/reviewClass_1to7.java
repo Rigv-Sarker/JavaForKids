cba236269f9506985fb03e469fcac191e4da5ac4
Rigv Sarker <Rigv.Sarker@gmail.com>
2025-01-08 18:26:36 -0600

    review

---
+++ src/reviewClasses/reviewClass_1to7.java
@@ -0,0 +1,21 @@
+package reviewClasses;
+
+public class reviewClass_1to7 {
+
+	public static void main(String[] args) {
+		int s = 10;
+		for (int i = 1; i <= s; i++) {
+			System.out.println(i);
+			
+			
+
+
+		}
+	}   
+}
+			
+	
+	
+	
+
+
